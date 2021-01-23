0197b32d0b80da2005e864f7ee6afb525f3bcf87
adam <brown.a2@gmail.com>
2021-01-23 17:21:19 +0000

    Patch - Invalid version constraint fix

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -18,11 +18,4 @@ terraform {
     bucket = "titan-project-b-tfstate"
     prefix = "env/dev"
   }
-
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "~> 3.53.0"
-    }
-  }
 }
