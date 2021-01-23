b4364ec17536f88813af4c3289fce544bb467dc8
adam <brown.a2@gmail.com>
2021-01-23 14:42:04 +0000

    Test new k8s terraform config

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,110 +1,41 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+resource "google_container_cluster" "default" {
+  name        = var.name
+  project     = var.project
+  description = "Demo GKE Cluster"
+  location    = var.location
 
+  remove_default_node_pool = true
+  initial_node_count       = var.initial_node_count
 
-locals {
-  env = "prod"
-}
+  master_auth {
+    username = ""
+    password = ""
 
-provider "google" {
-  project = var.project
-}
-
-# google_client_config and kubernetes provider must be explicitly specified like the following.
-data "google_client_config" "default" {}
-
-provider "kubernetes" {
-  load_config_file       = false
-  host                   = "https://${module.gke.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-}
-
-module "gke" {
-  source                     = "terraform-google-modules/kubernetes-engine/google"
-  project_id                 = var.project
-  name                       = "gke-test-1"
-  region                     = "us-central1"
-  zones                      = ["us-central1-a"]
-  network                    = "vpc-01"
-  subnetwork                 = "us-central1-01"
-  ip_range_pods              = "us-central1-01-gke-01-pods"
-  ip_range_services          = "us-central1-01-gke-01-services"
-  http_load_balancing        = false
-  horizontal_pod_autoscaling = true
-  network_policy             = true
-
-  node_pools = [
-    {
-      name               = "default-node-pool"
-      machine_type       = "e2-medium"
-      node_locations     = "us-central1-b,us-central1-c"
-      min_count          = 1
-      max_count          = 100
-      local_ssd_count    = 0
-      disk_size_gb       = 10
-      disk_type          = "pd-standard"
-      image_type         = "COS"
-      auto_repair        = true
-      auto_upgrade       = true
-      service_account    = "project-service-account@${var.project}.iam.gserviceaccount.com"
-      preemptible        = false
-      initial_node_count = 1
-    },
-  ]
-
-  node_pools_oauth_scopes = {
-    all = []
-
-    default-node-pool = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
-
-  node_pools_labels = {
-    all = {}
-
-    default-node-pool = {
-      default-node-pool = true
+    client_certificate_config {
+      issue_client_certificate = false
     }
   }
+}
 
-  node_pools_metadata = {
-    all = {}
+resource "google_container_node_pool" "default" {
+  name       = "${var.name}-node-pool"
+  project    = var.project
+  location   = var.location
+  cluster    = google_container_cluster.default.name
+  node_count = 1
 
-    default-node-pool = {
-      node-pool-metadata-custom-value = "my-node-pool"
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
+
+    metadata = {
+      disable-legacy-endpoints = "true"
     }
-  }
 
-  node_pools_taints = {
-    all = []
-
-    default-node-pool = [
-      {
-        key    = "default-node-pool"
-        value  = true
-        effect = "PREFER_NO_SCHEDULE"
-      },
-    ]
-  }
-
-  node_pools_tags = {
-    all = []
-
-    default-node-pool = [
-      "default-node-pool",
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
     ]
   }
 }
+
--- environments/prod/terraform.tfvars
+++
@@ -1,1 +0,0 @@
-project="titan-project-b"
